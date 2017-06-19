9d79c9b4229177a23ffddaf3dc02e541fe125612
Rory B-W <rory.batewilliams@gmail.com>
2017-06-19 16:56:34 +0100

    Update

--- js/materialize.js
+++ js/materialize.js
@@ -60,6 +60,21 @@ $(document).ready(function(){
       });
     });
 
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    return false;
+});
+
+var $root = $('html, body');
+$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
 /*$(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -68,13 +83,31 @@ $(document).ready(function(){
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 750);
+        }, 900);
         return false;
       }
     }
   });
 });*/
 
+/*// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);*/
 
 
 jQuery.extend( jQuery.easing,
